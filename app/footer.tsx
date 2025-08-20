07892c3dbe98759d3fb01387dd95855d45215621
Alfredo Zuniga <diaz.alfredo2001@gmail.com>
2025-08-19 22:24:07 -0500

    Customize footer

--- app/footer.tsx
+++ app/footer.tsx
@@ -72,8 +72,7 @@ export function Footer() {
       <div className="flex items-center justify-between">
         <a href="https://github.com/ibelick/nim" target="_blank">
           <TextLoop className="text-xs text-zinc-500">
-            <span>© 2024 Nim.</span>
-            <span>Built with Motion-Primitives.</span>
+            <span>© 2025 Alfredo Zuniga</span>
             <span>Customized by Alfredo Zuniga =)</span>
           </TextLoop>
         </a>
